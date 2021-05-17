1b8bf7a787d6ac0fcf30302a2a04eb617b181fd9
Alex Melnikov <kollege91@gmail.com>
2021-05-17 11:40:07 +0300

    task 2 complete

--- task_1/solution.js
+++ task_1/solution.js
@@ -1,49 +1,49 @@
 function addInCartMessage(title, price) {
-    let productName = title;
-    let productPrice = price;
+	 let productName = title;
+	 let productPrice = price;
 
-    // Задание №1.1. Оповещение о добавлении в корзину
+	 // Задание №1.1. Оповещение о добавлении в корзину
 
-    // создайте переменную message
-   let  message = title;                             // присвойте переменной message значение переменной productName
-   message = message + " ";                         // добавьте в конец переменной message пробел
-   message = message + "за";                       // добавьте в конец переменной message текст 'за'
-   message = message + " ";                        // <--Здесь еще один пробел забыли
-   message = message +  price;                    // добавьте в конец переменной message значение переменной productPrice
-   message = message + " ";                      // добавьте в конец переменной message пробел
-   message = message + "теперь в корзине!";     // добавьте в конец переменной message текст 'теперь в корзине!'
-   console.log(message);                      // выведите переменную message в консоль
+	 // создайте переменную message
+	let  message = title;                             // присвойте переменной message значение переменной productName
+	message = message + " ";                         // добавьте в конец переменной message пробел
+	message = message + "за";                       // добавьте в конец переменной message текст 'за'
+	message = message + " ";                        // <--Здесь еще один пробел забыли
+	message = message +  price;                    // добавьте в конец переменной message значение переменной productPrice
+	message = message + " ";                      // добавьте в конец переменной message пробел
+	message = message + "теперь в корзине!";     // добавьте в конец переменной message текст 'теперь в корзине!'
+	console.log(message);                      // выведите переменную message в консоль
 
-    // Конец решения задания №1.1.
+	 // Конец решения задания №1.1.
 
-    return message;
+	 return message;
 }
 
 function addInCartCountChange(value) {
 
-    // Задание №1.2. Увеличение количества в корзине
-    let oldValue = value;
+	 // Задание №1.2. Увеличение количества в корзине
+	 let oldValue = value;
 
-    let newValue = value + 1;              // создайте переменную newValue
-    // присвойте ей значение oldValue
-    // увеличьте значение newValue на 1
+	 let newValue = value + 1;              // создайте переменную newValue
+	 // присвойте ей значение oldValue
+	 // увеличьте значение newValue на 1
 
-    // Конец решения задания №1.2.
+	 // Конец решения задания №1.2.
 
-    return newValue;
+	 return newValue;
 }
 
 function addInCartSumChange(sum, delta) {
-    let oldSum = sum;
-    let difference = delta;
+	 let oldSum = sum;
+	 let difference = delta;
 
-    // Задание №1.3. Увеличение суммы в корзине
+	 // Задание №1.3. Увеличение суммы в корзине
 
-    //let newSum = (oldSum+difference)                    // создайте переменную newSum и присвойте ей сумму oldSum и difference
-   //let newSumText = newSum + " ₽"                      // создайте переменную newSumText и присвойте ей результат склеивания переменной newSum и текста ' ₽'
-    let newSumText = (`${oldSum + difference} ₽`);      // замените склеивание, используемое в newSum Text на шаблонную строку
+	 //let newSum = (oldSum+difference)                    // создайте переменную newSum и присвойте ей сумму oldSum и difference
+	//let newSumText = newSum + " ₽"                      // создайте переменную newSumText и присвойте ей результат склеивания переменной newSum и текста ' ₽'
+	 let newSumText = (`${oldSum + difference} ₽`);      // замените склеивание, используемое в newSum Text на шаблонную строку
 
-    // Конец решения задания №1.3.
+	 // Конец решения задания №1.3.
 
-    return newSumText;
+	 return newSumText;
 }
--- task_2/solution.js
+++ task_2/solution.js
@@ -1,64 +1,60 @@
 function calcShipping(sum, min, shipping) {
-    let productSum = sum; // сумма в корзине
-    let freeShippingMinSum = min; // минимальная цена для бесплатной доставки
-    let shippingPrice = shipping; // стоимость доставки
+	let productsSum = sum; // сумма в корзине <== (не хватаетало буквы "s")
+	let freeShippingMinSum = min; // минимальная цена для бесплатной доставки
+	let shippingPrice = shipping; // стоимость доставки
 
-    // Задание №2.1. Рассчитать доставку
+	// Задание №2.1. Рассчитать доставку
 
-    // создайте переменную shippingPrice
+	if (productsSum == 0) {												// если productsSum равно 0
+		shippingPrice = 0;												// то shippingPrice присвоить значение 0
+	}
+	else if (productsSum >= freeShippingMinSum) {			// если productsSum > или равен freeShippingMinSum,
+		shippingPrice = 0;											// то shippingPrice присвоить значение 0
+	}
 
-    // если productsSum равно 0,
-    // то shippingPrice присвоить значение 0
+	// Конец решения задания №2.1.
 
-    // если productsSum больше нуля, но меньше freeShippingMinSum,
-    // то shippingPrice присвоить значение shipping
-
-    // если productsSum > или равен freeShippingMinSum,
-    // то shippingPrice присвоить значение 0
-
-    // Конец решения задания №2.1.
-
-    return shippingSum;
+	return shippingPrice;
 }
 
 function calcDiscount(sum, min, discount) {
-    let productsSum = sum; // сумма в корзине
-    let discountMinSum = min; // минимальная цена для скидки
-    let discountPart = discount; // величина скидки в процентах
+	let productsSum = sum; // сумма в корзине
+	let discountMinSum = min; // минимальная цена для скидки
+	let discountPart = discount; // величина скидки в процентах
 
-    // Задание №2.2. Рассчитать скидку
+	// Задание №2.2. Рассчитать скидку
 
-    // создайте переменную discountPrice
+	let discountSum = (productsSum >= discountMinSum) ? (productsSum / 100) * discountPart : 0;		// создайте переменную discountPrice <== Неправильное название переменной
 
-    // если productsSum больше или равно discountMinSum,
-    // то присвойте discountPrice значение discountPart процентов от productsSum,
-    // иначе присвойте discountPrice значения 0
+	// если productsSum больше или равно discountMinSum,
+	// то присвойте discountPrice значение discountPart процентов от productsSum,
+	// иначе присвойте discountPrice значения 0
 
-    // Конец решения задания №2.2.
+	// Конец решения задания №2.2.
 
-    return discountSum;
+	return discountSum;
 }
 
 function calcInvoice({sum, discountMinSum, discountPart, shippingFreeMinSum, shippingPrice}) {
-    let productsSum = sum;
-    let discountSum = calcDiscount(sum, discountMinSum, discountPart);
+	let productsSum = sum;
+	let discountSum = calcDiscount(sum, discountMinSum, discountPart);
 
-    // Задача №2.3. Рассчитать скидки и доставку в корзине
+	// Задача №2.3. Рассчитать скидки и доставку в корзине
 
-    // создайте переменную totalSum
+	let totalSum = productsSum - discountSum;							// создайте переменную totalSum
 
-    // присвойте totalSum значение productsSum
-    // уменьшите totalSum на discountSum
+	// присвойте totalSum значение productsSum
+	// уменьшите totalSum на discountSum
 
-    let shippingSum = calcShipping(totalSum, shippingFreeMinSum, shippingPrice); // не изменяйте эту строку!!!
+	let shippingSum = calcShipping(totalSum, shippingFreeMinSum, shippingPrice); // не изменяйте эту строку!!!
 
-    // прибавьте к totalSum значение shippingSum
+	totalSum = totalSum + shippingSum;												// прибавьте к totalSum значение shippingSum
 
-    // создайте переменную freeShipping
-    // запишите без использования if или любых других условий:
-    // если shippingSum равно нулю, то freeShipping должна быть равна true, иначе freeShipping должна быть равна false
+	let freeShipping;																	// создайте переменную freeShipping
+	// запишите без использования if или любых других условий:
+	freeShipping = shippingSum == 0;				// если shippingSum равно нулю, то freeShipping должна быть равна true, иначе freeShipping должна быть равна false
 
-    // Конец решения задачи №2.3.
+	// Конец решения задачи №2.3.
 
-    return {discount: discountSum, freeShipping, shipping: shippingSum, total: totalSum};
+	return {discount: discountSum, freeShipping, shipping: shippingSum, total: totalSum};
 }
